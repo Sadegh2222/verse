2d88830e675d561b0f1e33aa5e31ac83b84c3644
Sadegh2222 <163021946+Sadegh2222@users.noreply.github.com>
2025-05-12 13:27:09 +0330

    Update download.py

--- download.py
+++ download.py
@@ -1,16 +1,9 @@
-import os
-from spotdl import Spotdl
-from dotenv import load_dotenv
+import subprocess
 
-load_dotenv()
-
-def download_song(song_name):
-    # Initialize SpotDL and download the song
-    sp = Spotdl()
-    song = sp.search(song_name)
-
-    if not song:
-        return None
-
-    song_path = sp.download(song)
-    return song_path
+def download_song(query):
+    try:
+        # اجرای SpotDL برای دانلود آهنگ
+        result = subprocess.run(["spotdl", query], capture_output=True, text=True, timeout=60)
+        return result.stdout  # مسیر فایل دانلود شده
+    except Exception as e:
+        return str(e)
